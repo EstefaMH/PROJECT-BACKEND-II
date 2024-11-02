f19e1b74f0884d3b0c76c618a49edaaef16a6ecd
EstefaMH <estefymoncaleano@gmail.com>
2024-11-02 16:33:56 -0500

    update products-section styles

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -13,39 +13,39 @@ socket.on("newProduct", product => {
     const productCard = document.getElementById("section-products-realtime")
     console.log("new")
     const article = document.createElement("article");
-            article.id = "product";
-            article.classList.add("product");
-            productCard.append(article);
+    article.id = "product";
+    article.classList.add("product");
+    productCard.append(article);
 
-            const title = document.createElement("h2");
-            title.textContent = product.title;
+    const title = document.createElement("h2");
+    title.textContent = product.title;
 
-            const description = document.createElement("p");
-            description.textContent =  product.description;
+    const description = document.createElement("p");
+    description.textContent = product.description;
 
-            const code = document.createElement("p");
-            code.textContent = `Código: ${ product.code}`;
+    const code = document.createElement("p");
+    code.textContent = `Código: ${product.code}`;
 
-            const price = document.createElement("p");
-            price.textContent = `Precio: ${ product.price}`;
+    const price = document.createElement("p");
+    price.textContent = `Precio: ${product.price}`;
 
-            const stock = document.createElement("p");
-            stock.textContent = `Stock: ${ product.stock}`;
+    const stock = document.createElement("p");
+    stock.textContent = `Stock: ${product.stock}`;
 
-            const category = document.createElement("p");
-            category.textContent = `Categoría: ${ product.category}`;
+    const category = document.createElement("p");
+    category.textContent = `Categoría: ${product.category}`;
 
-            article.appendChild(title);
-            article.appendChild(description);
-            article.appendChild(code);
-            article.appendChild(price);
-            article.appendChild(stock);
-            article.appendChild(category);
+    article.appendChild(title);
+    article.appendChild(description);
+    article.appendChild(code);
+    article.appendChild(price);
+    article.appendChild(stock);
+    article.appendChild(category);
 })
 
 
 const getProducts = async () => {
-    
+
     try {
         const productCard = document.getElementById("section-products-realtime")
         const response = await fetch("/api/products");
@@ -66,24 +66,28 @@ const getProducts = async () => {
             article.classList.add("product");
             productCard.append(article);
 
+            const image = document.createElement("img");
+            image.src = product.thumbnails;
+
             const title = document.createElement("h2");
             title.textContent = product.title;
 
             const description = document.createElement("p");
-            description.textContent =  product.description;
+            description.textContent = product.description;
 
             const code = document.createElement("p");
-            code.textContent = `Código: ${ product.code}`;
+            code.textContent = `Código: ${product.code}`;
 
             const price = document.createElement("p");
-            price.textContent = `Precio: ${ product.price}`;
+            price.textContent = `Precio: ${product.price}`;
 
             const stock = document.createElement("p");
-            stock.textContent = `Stock: ${ product.stock}`;
+            stock.textContent = `Stock: ${product.stock}`;
 
             const category = document.createElement("p");
-            category.textContent = `Categoría: ${ product.category}`;
+            category.textContent = `Categoría: ${product.category}`;
 
+            article.appendChild(image);
             article.appendChild(title);
             article.appendChild(description);
             article.appendChild(code);
@@ -91,8 +95,8 @@ const getProducts = async () => {
             article.appendChild(stock);
             article.appendChild(category);
 
-            
-            
+
+
         });
     } catch (error) {
         console.error("Error :", error);
