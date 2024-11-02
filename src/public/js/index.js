31fb71491c95603a460a30c8e85638babd3af4d3
EstefaMH <estefymoncaleano@gmail.com>
2024-11-02 18:37:34 -0500

    update index & data

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -7,15 +7,24 @@ socket.on("saludo", datos => {
     socket.emit("id", "user 123333")
 })
 
+socket.on("deleteProduct", datos => {
+
+     const productCard = document.getElementById(datos);
+     productCard.remove()
+   
+})
 
 
 socket.on("newProduct", product => {
     const productCard = document.getElementById("section-products-realtime")
-    console.log("new")
+ 
     const article = document.createElement("article");
-    article.id = "product";
+    article.id = product.id;
     article.classList.add("product");
     productCard.append(article);
+
+    const image = document.createElement("img");
+    image.src = product.thumbnails;
 
     const title = document.createElement("h2");
     title.textContent = product.title;
@@ -35,71 +44,21 @@ socket.on("newProduct", product => {
     const category = document.createElement("p");
     category.textContent = `Categoría: ${product.category}`;
 
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Eliminar";
+    removeButton.addEventListener("click", () => {
+        deleteProduct(product.id); 
+    });
+    
+    article.appendChild(image);
     article.appendChild(title);
     article.appendChild(description);
     article.appendChild(code);
     article.appendChild(price);
     article.appendChild(stock);
     article.appendChild(category);
+    article.appendChild(removeButton);
 })
 
 
-const getProducts = async () => {
 
-    try {
-        const productCard = document.getElementById("section-products-realtime")
-        const response = await fetch("/api/products");
-
-        if (!response.ok) {
-            throw new Error(`API request failed with status ${response.status}`);
-        }
-
-        const data = await response.json();
-        const productos = data.data;
-
-        console.log("Products cargados exitosamente!", productos);
-
-        productos.forEach((product) => {
-
-            const article = document.createElement("article");
-            article.id = "product";
-            article.classList.add("product");
-            productCard.append(article);
-
-            const image = document.createElement("img");
-            image.src = product.thumbnails;
-
-            const title = document.createElement("h2");
-            title.textContent = product.title;
-
-            const description = document.createElement("p");
-            description.textContent = product.description;
-
-            const code = document.createElement("p");
-            code.textContent = `Código: ${product.code}`;
-
-            const price = document.createElement("p");
-            price.textContent = `Precio: ${product.price}`;
-
-            const stock = document.createElement("p");
-            stock.textContent = `Stock: ${product.stock}`;
-
-            const category = document.createElement("p");
-            category.textContent = `Categoría: ${product.category}`;
-
-            article.appendChild(image);
-            article.appendChild(title);
-            article.appendChild(description);
-            article.appendChild(code);
-            article.appendChild(price);
-            article.appendChild(stock);
-            article.appendChild(category);
-
-
-
-        });
-    } catch (error) {
-        console.error("Error :", error);
-    }
-}
-getProducts()
