3d0bdc893f8a903414a3b92c6a4daf27bf396feb
EstefaMH <estefymoncaleano@gmail.com>
2024-11-18 03:52:13 -0500

    modified product services

--- src/public/services/productsServices.js
+++ src/public/services/productsServices.js
@@ -4,14 +4,15 @@ const getProducts = async () => {
         const productCard = document.getElementById("section-products-realtime")
         const response = await fetch("/api/products");
 
+
         if (!response.ok) {
             throw new Error(`API request failed with status ${response.status}`);
         }
 
         const data = await response.json();
-        const productos = data.data;
+        const productos = data;
 
-        console.log("Products cargados exitosamente!", productos);
+        console.log("Objetos que retorna el GET", data);
 
         productos.forEach((product) => {
 
@@ -19,35 +20,35 @@ const getProducts = async () => {
             article.id = product.id;
             article.classList.add("product");
             productCard.append(article);
-        
+
             const image = document.createElement("img");
             image.src = product.thumbnails;
-        
+
             const title = document.createElement("h2");
             title.textContent = product.title;
-        
+
             const description = document.createElement("p");
             description.textContent = product.description;
-        
+
             const code = document.createElement("p");
             code.textContent = `Código: ${product.code}`;
-        
+
             const price = document.createElement("p");
             price.textContent = `Precio: ${product.price}`;
-        
+
             const stock = document.createElement("p");
             stock.textContent = `Stock: ${product.stock}`;
-        
+
             const category = document.createElement("p");
             category.textContent = `Categoría: ${product.category}`;
 
             const removeButton = document.createElement("button");
             removeButton.textContent = "Eliminar";
             removeButton.addEventListener("click", () => {
-                deleteProduct(product.id); 
+                deleteProduct(product.id);
             });
-            
-            
+
+
             article.appendChild(image);
             article.appendChild(title);
             article.appendChild(description);
@@ -64,19 +65,27 @@ const getProducts = async () => {
 }
 getProducts()
 
-const deleteProduct = async (pid) =>{
+const deleteProduct = async (pid) => {
     try {
         const response = await fetch(`/api/products/${pid}`, {
             method: 'DELETE'
         });
-    
+
         if (!response.ok) {
             throw new Error(`API request failed with status ${response.status}`);
         }
-    
+
         const data = await response.json();
         console.log("Product deleted successfully:", data);
     } catch (error) {
         console.error("Error deleting product:", error);
     }
 }
+
+Handlebars.registerHelper('postProductoToCart', async function (productId, quantity) {
+    const formData = new FormData();
+    formData.append('productId', productId);
+    formData.append('quantity', quantity);
+  
+   
+  });
