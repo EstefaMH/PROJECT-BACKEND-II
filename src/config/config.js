6ddcea480c17cf3b69f9f5decc2aad5a83732316
EstefaMH <estefymoncaleano@gmail.com>
2024-11-18 03:50:42 -0500

    modified config

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,10 @@ export const config = {
     dataFiles: {
         products: process.env.PRODUCTS_PATH,
         cart: process.env.CART_PATH,
+    },
+    dataBase:{
+        mongoUrl: process.env.MONGODB_URL,
+        mongoDataBaseName : process.env.MONGODB_NAME
     }
 
 }
